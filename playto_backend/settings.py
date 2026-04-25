a2e67f9fb3d9d471d9f6a66fb3774f1fd2dbd936
Test User <test@example.com>
2026-04-26 02:04:28 +0530

    Fix ALLOWED_HOSTS for Render deployment

--- playto_backend/settings.py
+++ playto_backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-nyhq!+blpllzyc8cnl!e%_u)t76-40hxg4c9-_gc&tt57w#-xv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
